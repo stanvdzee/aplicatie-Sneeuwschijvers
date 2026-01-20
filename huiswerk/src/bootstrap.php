495564be1decc16904dcadf2a947c40b00cb8676
stanvdzee <511111@student.firda.nl>
2026-01-20 09:37:29 +0100

    huiswerk

--- huiswerk/src/bootstrap.php
+++ huiswerk/src/bootstrap.php
@@ -2,8 +2,33 @@
 
 declare(strict_types=1);
 
+use GuzzleHttp\Psr7\ServerRequest;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Utils;
+use HttpSoft\HttpEmitter\SapiEmitter;
+
 ini_set('display_errors', '1');
 
-$page = $_GET['page'];
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$request = ServerRequest::fromGlobals();
+
+$page = $request->getQueryParams()['page'];
+
+ob_start();
 
 require dirname(__DIR__) . "/{$page}.php";
+
+$content = ob_get_clean();
+
+$stream = Utils::streamFor($content);
+
+$response = new Response();
+
+$response = $response->withStatus(418)
+                        ->withHeader("X-Powered-By", "PHP")
+                        ->withBody($stream);
+
+$emitter = new SapiEmitter();
+
+$emitter->emit($response);
